28f2135d60495e5c3f42409d4ff9020d86849517
DenisuDevForFood <denisfeier98@yahoo.com>
2020-07-13 16:59:45 +0300

    recursive scanner reads all attributes for every file in a project

---
+++ src/main/java/com/denisfeier/RecursiveScanner.java
@@ -0,0 +1,37 @@
+package com.denisfeier;
+
+import com.denisfeier.entity.FileAttributes;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecursiveScanner {
+
+    private final List<FileAttributes> paths = new ArrayList<>();
+
+    private List<FileAttributes> walk(Path path) throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    walk(entry);
+                }
+
+                BasicFileAttributes attr = Files.readAttributes(entry, BasicFileAttributes.class);
+
+                this.paths.add(new FileAttributes(entry));
+            }
+        }
+        return this.paths;
+    }
+
+    public static List<FileAttributes> dirScanning(Path path) throws IOException {
+        RecursiveScanner scanner = new RecursiveScanner();
+        return scanner.walk(path);
+    }
+
+}
