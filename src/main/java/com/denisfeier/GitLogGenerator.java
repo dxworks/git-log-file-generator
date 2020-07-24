bd44dadef28e823d71892983e444813b0b70be16
DenisuDevForFood <denisfeier98@yahoo.com>
2020-07-25 01:41:02 +0300

    test read blacklist file

--- src/main/java/com/denisfeier/GitLogGenerator.java
+++ src/main/java/com/denisfeier/GitLogGenerator.java
@@ -1,5 +1,8 @@
 package com.denisfeier;
 
+import com.denisfeier.blacklist.BlackListReader;
+import com.denisfeier.blacklist.BlackListReaderFile;
+import com.denisfeier.blacklist.entity.BlackListEntity;
 import com.denisfeier.entity.FileAttributes;
 
 import java.nio.file.Path;
@@ -9,16 +12,23 @@ import java.util.List;
 public class GitLogGenerator {
     public static void main(String[] args) {
         String stringPath = args[0];
-
-        Path path = Paths.get(stringPath);
-        try {
-            List<FileAttributes> fileAttributes = RecursiveScanner.dirScanning(path);
-            for (FileAttributes entity: fileAttributes) {
-                System.out.println(entity);
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
+        String blackListPath = args[1];
+        System.out.println(stringPath);
+        System.out.println(blackListPath);
+        BlackListReader reader = new BlackListReaderFile();
+        for (BlackListEntity str: reader.createBlackList(blackListPath)) {
+            System.out.println(str);
         }
+
+//        Path path = Paths.get(stringPath);
+//        try {
+//            List<FileAttributes> fileAttributes = RecursiveScanner.dirScanning(path);
+//            for (FileAttributes entity: fileAttributes) {
+//                System.out.println(entity);
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
     }
 }
