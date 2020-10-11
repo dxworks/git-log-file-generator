71a7a2daa608a936a1744d934138fe79bfdb91f5
DenisuDevForFood <denisfeier98@yahoo.com>
2020-10-11 21:37:20 +0300

    add config

---
+++ src/main/java/com/denisfeier/config/Config.java
@@ -0,0 +1,27 @@
+package com.denisfeier.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Config {
+    public Threshold threshold;
+    public String rootDir;
+
+    public static Config createConfig(Path path) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        return mapper.readValue(path.toFile(), Config.class);
+    }
+}
