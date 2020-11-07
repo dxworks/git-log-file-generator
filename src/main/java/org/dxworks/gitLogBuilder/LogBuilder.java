43b9164cb89a2b36d8234fa616ecf15689cf0f0c
DenisuDevForFood <denisfeier98@yahoo.com>
2020-11-07 16:20:43 +0200

    refactoring and add write in files

--- src/main/java/org/dxworks/gitLogBuilder/LogBuilder.java
+++ src/main/java/org/dxworks/gitLogBuilder/LogBuilder.java
@@ -1,26 +1,33 @@
 package org.dxworks.gitLogBuilder;
 
+import org.dxworks.config.Config;
 import org.dxworks.entity.FileAttribute;
+import org.dxworks.entity.GitCommit;
+import org.dxworks.exception.EmptyAttributesListException;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class LogBuilder {
 
+    private final static String pattern = "yyyy-MM-dd HH:mm:ss";
+    private final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
     public static Map<Long, List<FileAttribute>> sortedByLastModifiedTime(List<FileAttribute> fileAttributes, int hours, int minutes, int seconds) {
 
         int calculatedHours = hours * 3600;
         int calculatedMinutes = minutes * 60;
 
-//        System.out.println(hours);
-//        System.out.println(minutes);
-//        System.out.println(seconds);
-
         long step = (calculatedHours <= 0
                 ? 1 : calculatedHours) * (calculatedMinutes <= 0
                 ? 1 : calculatedMinutes) * (seconds <= 0
-                ? 1 : seconds) * 1000;
+                ? 1 : seconds);
 
         System.out.println("Step:" + step);
 
@@ -28,10 +35,10 @@ public class LogBuilder {
                 Comparator.comparing(FileAttribute::getLastModifiedTime)
         ).collect(Collectors.toList());
 
-        Map<Long, List<FileAttribute>> sortedMap = new HashMap<>();
+        Map<Long, List<FileAttribute>> sortedMap = new TreeMap<>();
 
         for (FileAttribute f : sortedList) {
-            long key = f.getLastModifiedTime().toInstant(ZoneOffset.UTC).toEpochMilli() / step;
+            long key = (f.getLastModifiedTime().toInstant(ZoneOffset.UTC).toEpochMilli() / 1000) / step;
 
             if (sortedMap.containsKey(key)) {
                 sortedMap.get(key).add(f);
@@ -45,6 +52,72 @@ public class LogBuilder {
         return sortedMap;
     }
 
+    public static List<FileAttribute> getModifiedFilesOnly(List<FileAttribute> fileAttributeList) {
+        return fileAttributeList
+                .stream()
+                .filter(fileAttribute -> {
+                    String formatted1 = simpleDateFormat.format(new Date(fileAttribute.getLastModifiedTime().toInstant(OffsetDateTime.now().getOffset()).toEpochMilli()));
+                    String formatted2 = simpleDateFormat.format(new Date(fileAttribute.getCreationTime().toInstant(OffsetDateTime.now().getOffset()).toEpochMilli()));
+                    return !formatted1.equals(formatted2);
+                }
+                ).collect(Collectors.toList());
+    }
 
+    private static Map<Long, List<FileAttribute>> configuredSortedListOfFileAttribute(Config config, List<FileAttribute> fileAttributes) {
 
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+
+        if (config.threshold != null) {
+            hours = config.threshold.getHours();
+            minutes = config.threshold.getMinutes();
+            seconds = config.threshold.getSeconds();
+        }
+
+        return LogBuilder.sortedByLastModifiedTime(
+                fileAttributes,
+                hours,
+                minutes,
+                seconds);
+    }
+
+    public static List<GitCommit> createGitCommitLogs(List<FileAttribute> listOfFiles, Config config, boolean added) throws EmptyAttributesListException {
+        Map<Long, List<FileAttribute>> sortedByLastModifiedTime =
+                configuredSortedListOfFileAttribute(config, listOfFiles);
+
+        List<GitCommit> gitCommits = new LinkedList<>();
+
+        for (Map.Entry<Long, List<FileAttribute>> entry : sortedByLastModifiedTime.entrySet()) {
+            GitCommit gitCommit = new GitCommit(entry.getValue());
+            gitCommit.setAdded(added);
+            gitCommits.add(gitCommit);
+        }
+
+        return gitCommits;
+    }
+
+    public static void showCommits(List<FileAttribute> attributeList, Config config, boolean added) throws EmptyAttributesListException {
+        if (attributeList.size() != 0) {
+            List<GitCommit> gitCommitsForJustAdded = LogBuilder.createGitCommitLogs(attributeList, config, added);
+
+            gitCommitsForJustAdded.forEach(gitCommit -> System.out.println(gitCommit.stringCommit()));
+        }
+    }
+
+    public static void outputInFile(Path outputPath, List<FileAttribute> attributeList, Config config, boolean added) throws EmptyAttributesListException, IOException {
+        if (attributeList.size() != 0) {
+            List<GitCommit> gitCommitsForJustAdded = LogBuilder.createGitCommitLogs(attributeList, config, added);
+
+            FileWriter myWriter = new FileWriter(outputPath.toFile());
+            gitCommitsForJustAdded.forEach(gitCommit -> {
+                try {
+                    myWriter.write(gitCommit.stringCommit());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            myWriter.close();
+        }
+    }
 }
