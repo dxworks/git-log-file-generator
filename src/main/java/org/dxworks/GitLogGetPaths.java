8a46670d7bb787a8ceee9a62699bf83de55213b2
DenisuDevForFood <denisfeier98@yahoo.com>
2020-11-07 17:01:50 +0200

    refactoring and add output to file

--- src/main/java/org/dxworks/GitLogGetPaths.java
+++ src/main/java/org/dxworks/GitLogGetPaths.java
@@ -1,50 +1,67 @@
 package org.dxworks;
 
 import org.dxworks.config.Config;
+import org.dxworks.config.interpreter.ConfigInterpreter;
 import org.dxworks.entity.FileAttribute;
-import org.dxworks.entity.GitCommit;
+import org.dxworks.exception.NoProjectPathException;
 import org.dxworks.gitLogBuilder.LogBuilder;
 import org.dxworks.ignorer.Ignorer;
 import org.dxworks.ignorer.IgnorerBuilder;
 import org.dxworks.scanner.RecursiveScanner;
 
 import java.nio.file.Path;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public class GitLogGetPaths {
 
     public static void main(String[] args) throws Exception {
 
-        String projectPath = "/home/denisu/IdeaProjects/GitLogGenerator";
-
-        IgnorerBuilder builder = new IgnorerBuilder(
-                Path.of("/home/denisu/IdeaProjects/GitLogGenerator/src/main/resources/.globs1"));
-
-        Ignorer ignorer = builder.compile();
-
-        List<FileAttribute> list =
-                RecursiveScanner.dirScanning(
-                        Path.of(projectPath),
-                        Optional.of(ignorer),
-                        Optional.of(Path.of(projectPath)));
-
-        Map<Long, List<FileAttribute>> sortedByLastModifiedTime =
-                LogBuilder.sortedByLastModifiedTime(list, 300, 0, 0);
-
-        List<GitCommit> gitCommits = new LinkedList<>();
-
-        for (Map.Entry<Long, List<FileAttribute>> entry : sortedByLastModifiedTime.entrySet()) {
-           gitCommits.add(new GitCommit(entry.getValue()));
+        if (System.getProperty("projectPath", "").equals("")) {
+            throw new NoProjectPathException("We couldn't find the path of your project");
         }
 
-//        gitCommits.forEach(gitCommit -> System.out.println(gitCommit.stringCommit()));
+        Config config = ConfigInterpreter.getFullConfig();
 
-        Config config = Config.createConfig(Path.of("/home/denisu/IdeaProjects/GitLogGenerator/src/main/resources/config"));
+        String projectPath = System.getProperty("projectPath");
 
-        System.out.println(config);
+        List<FileAttribute> listOfFiles =
+                RecursiveScanner.dirScanning(
+                        Path.of(projectPath),
+                        getIgnorer(),
+                        relativeTo(config));
 
+        List<FileAttribute> listOfModified = LogBuilder.getModifiedFilesOnly(listOfFiles);
+
+        if (System.getProperty("outputFile", "").equals("")) {
+            LogBuilder.showCommits(listOfFiles, config, true);
+            LogBuilder.showCommits(listOfModified, config, false);
+        } else {
+            Path outputPath = Path.of(System.getProperty("outputFile"));
+            LogBuilder.outputInFile(outputPath, listOfFiles, config, true);
+            LogBuilder.outputInFile(outputPath, listOfModified, config, false);
+        }
     }
+
+    static private Optional<Ignorer> getIgnorer() {
+
+        if (System.getProperty("ignorer", "").equals("")) {
+            return Optional.empty();
+        } else {
+            IgnorerBuilder builder = new IgnorerBuilder(
+                    Path.of(System.getProperty("ignorer")));
+
+            Ignorer ignorer = builder.compile();
+
+            return Optional.of(ignorer);
+        }
+    }
+
+    static private Optional<Path> relativeTo(Config config) {
+        return config.rootDir != null ?
+                Optional.of(Path.of(config.rootDir))
+                :
+                Optional.empty();
+    }
+
 }
