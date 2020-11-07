64cdedf810fc153d5f3a6f3c3489da18b718f656
DenisuDevForFood <denisfeier98@yahoo.com>
2020-11-07 16:17:15 +0200

    add object to read config from terminal or config file

---
+++ src/main/java/org/dxworks/config/interpreter/ConfigInterpreter.java
@@ -0,0 +1,29 @@
+package org.dxworks.config.interpreter;
+
+import org.dxworks.config.Config;
+import org.dxworks.config.Threshold;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class ConfigInterpreter {
+
+    public static Config getFullConfig() throws IOException {
+        if (System.getProperty("fileConfig") != null) {
+            Path ymlConfigPath = Path.of(System.getProperty("fileConfig"));
+            Config config = Config.createConfig(ymlConfigPath);
+            return config;
+        } else {
+            int hours = Integer.parseInt(System.getProperty("config.threshold.hours", "0"));
+            int minutes = Integer.parseInt(System.getProperty("config.threshold.minutes", "0"));
+            int seconds = Integer.parseInt(System.getProperty("config.threshold.seconds", "0"));
+            String rootDir = System.getProperty("config.rootDir");
+            Threshold threshold = new Threshold(hours, minutes, seconds);
+            return new Config(threshold, rootDir);
+        }
+
+
+//        return Config();
+    }
+
+}
