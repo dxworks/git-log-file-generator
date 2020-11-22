b6e9d1466f86e1fc0ea3c4d9ea1286af332035c6
Denis Feier <denisfeier98@yahoo.com>
2020-11-23 00:45:34 +0200

    clean up

--- src/main/java/org/dxworks/logGenerator/GitLogGetPaths.java
+++ src/main/java/org/dxworks/logGenerator/GitLogGetPaths.java
@@ -4,12 +4,14 @@ import org.dxworks.logGenerator.config.Config;
 import org.dxworks.logGenerator.config.interpreter.ConfigInterpreter;
 import org.dxworks.logGenerator.entity.FileAttribute;
 import org.dxworks.logGenerator.entity.GitCommit;
+import org.dxworks.logGenerator.exception.EmptyAttributesListException;
 import org.dxworks.logGenerator.exception.NoProjectPathException;
 import org.dxworks.logGenerator.gitLogBuilder.LogBuilder;
 import org.dxworks.logGenerator.ignorer.Ignorer;
 import org.dxworks.logGenerator.ignorer.IgnorerBuilder;
 import org.dxworks.logGenerator.scanner.RecursiveScanner;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
@@ -38,29 +40,9 @@ public class GitLogGetPaths {
                 RecursiveScanner.dirScanning(
                         Paths.get(projectPath),
                         getIgnorer(),
-                        relativeTo(config));
+                        relativeTo(projectPath));
 
-        List<FileAttribute> listOfModified = LogBuilder.getModifiedFilesOnly(listOfFiles);
-
-        if (System.getProperty("outputFile", "").equals("")) {
-            LogBuilder.showCommits(listOfFiles, config, true);
-            LogBuilder.showCommits(listOfModified, config, false);
-        } else {
-            Path outputPath = Paths.get(System.getProperty("outputFile"));
-
-            List<GitCommit> gitCommitsAdded = LogBuilder.createGitCommitLogs(listOfFiles, config, true);
-
-            List<GitCommit> gitCommitsModified = LogBuilder.createGitCommitLogs(listOfModified, config, false);
-
-            List<GitCommit> allCommits = new LinkedList<>();
-
-            allCommits.addAll(gitCommitsAdded);
-            allCommits.addAll(gitCommitsModified);
-
-            allCommits.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
-
-            LogBuilder.outputInFile(outputPath, allCommits);
-        }
+        outputCommits(listOfFiles, config);
     }
 
     static private Optional<Ignorer> getIgnorer() {
@@ -77,11 +59,29 @@ public class GitLogGetPaths {
         }
     }
 
-    static private Optional<Path> relativeTo(Config config) {
-        return config.rootDir != null ?
-                Optional.of(Paths.get(config.rootDir))
+    static private Optional<Path> relativeTo(String basePath) {
+        return !basePath.isEmpty() ?
+                Optional.of(Paths.get(basePath))
                 :
                 Optional.empty();
     }
 
+    static private void outputCommits(List<FileAttribute> listOfFiles, Config config) throws EmptyAttributesListException, IOException {
+
+        List<FileAttribute> listOfModified = LogBuilder.getModifiedFilesOnly(listOfFiles);
+        List<GitCommit> gitCommitsAdded = LogBuilder.createGitCommitLogs(listOfFiles, config, true);
+        List<GitCommit> gitCommitsModified = LogBuilder.createGitCommitLogs(listOfModified, config, false);
+        List<GitCommit> allCommits = new LinkedList<>();
+        allCommits.addAll(gitCommitsAdded);
+        allCommits.addAll(gitCommitsModified);
+
+        allCommits.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+
+        if (System.getProperty("outputFile", "").equals("")) {
+            LogBuilder.showCommits(allCommits);
+        } else {
+            Path outputPath = Paths.get(System.getProperty("outputFile"));
+            LogBuilder.outputInFile(outputPath, allCommits);
+        }
+    }
 }
